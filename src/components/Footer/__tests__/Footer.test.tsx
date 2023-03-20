1db70da640a07589cf91e06b6df97a81f38dac54
ALEKSAS LUKAŠEVIČIUS <aleksas.lukasevicius@codeacademylt.onmicrosoft.com>
2023-03-20 19:48:32 +0200

    ABC-117: add footer with current year

---
+++ src/components/Footer/__tests__/Footer.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import { Footer } from "../Footer";
+import renderer from "react-test-renderer";
+
+describe("Footer", () => {
+  it("matches snapshot", () => {
+    const tree = renderer.create(<Footer />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("should Foooter render", () => {
+    render(<Footer />);
+  });
+
+  it("should Footer render current year", () => {
+    render(<Footer />);
+    const currentYearElement = screen.getByLabelText("current-year");
+
+    expect(currentYearElement).toBeInTheDocument();
+    expect(currentYearElement.textContent).toBe(`${new Date().getFullYear()}`);
+  });
+});
